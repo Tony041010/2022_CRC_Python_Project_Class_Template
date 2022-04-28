c10b8e2bf757d93c6effa80f0c2f626ebc0d60d2
unknown <tony931010@gmail.com>
2022-04-28 16:52:37 +0800

    The third and fourth templates of the class : TicTacToe

---
+++ TicTacToe/game.py
@@ -0,0 +1,64 @@
+class TicTacToe:
+    def __init__(self):
+        pass
+        
+
+    def print_board(self):
+        pass
+    
+    @staticmethod
+    def print_board_nums():
+        # it's going to print 0 | 1 | 2 , etc (tell us what number corresponds to what position)
+        pass
+    
+
+    def available_moves(self):
+        # it'll return a list
+        pass
+
+    def empty_squares(self):
+        pass
+
+    def num_empty_squares(self):
+        pass
+
+    def make_move(self, square, letter):
+        # If valid move, them make the move (assign square to letter) then return True. 
+        # If invalid, then return False.
+        pass
+
+    def winner(self, square, letter):
+        # wins if 3 in a row anythere. We have to check all of these
+        pass
+        
+        # first we check the row
+        
+        
+        # check column
+        
+        
+        # check diagonals
+        # but if the square is an even umber(0, 2, 4, 6, 8)
+        # these are the only moves possible to win a diagonal
+        
+
+        # If all of them fail
+
+
+
+
+def play(game, x_player, o_player, print_game = True):
+    # return the winner of the game(the letter)! or None for a tie
+    pass
+
+    # iterate while the game still has empty squares
+    #(we don't have to worry about winner because we'll just return that
+    # which breaks the loop)
+
+    
+
+
+
+
+if __name__ == '__main__':
+   pass 
